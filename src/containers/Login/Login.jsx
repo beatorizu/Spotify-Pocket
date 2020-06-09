b3985155fcc334f4a3d3933233ea9df9667440a7
beatorizu <betrizaf@gmail.com>
2020-06-09 18:07:49 -0300

    update: Add login components

--- src/containers/Login/Login.jsx
+++ src/containers/Login/Login.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import { Logo } from '../../components'
+
 import './Login.scss';
 
-const Login = () => (<main className="login" />);
+const Login = () => (
+  <main className="login" data-testid="login">
+    <div className="container">
+      <Logo />
+      <h2 className="login__microcopy">
+        Não toca a música inteira, <strong>mas toca o seu <span role="img" className="login__microcopy__heart" aria-label="Coração">❤️</span></strong>
+      </h2>
+      <button className="login__auth-button">Entrar com Spotify</button>
+    </div>
+  </main>
+);
 
 export default Login;
