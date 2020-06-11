e6fef3a42832735e5fc3e757c952046cb6dd7822
beatorizu <betrizaf@gmail.com>
2020-06-11 19:59:04 -0300

    feat: Add authorize route

---
+++ src/routes/AuthorizeRoute.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const AuthorizeRoute = () => <div></div>;
+
+export default AuthorizeRoute;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import LoginRoute from './LoginRoute'
+import AuthorizeRoute from './AuthorizeRoute'
 
 const Routes = () => (
   <Switch>
     <Route exact path="/">
       <LoginRoute />
+    </Route>
+    <Route exact path="/authorize">
+      <AuthorizeRoute />
     </Route>
   </Switch>
 );
