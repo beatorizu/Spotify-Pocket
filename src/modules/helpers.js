4e1f3b9bfe7b792ca67072b9b81212404e03fc0b
beatorizu <betrizaf@gmail.com>
2020-06-14 20:16:27 -0300

    feat: Get content by ID

--- src/modules/helpers.js
+++ src/modules/helpers.js
@@ -1,1 +1,7 @@
-export const getContentNameById = (categoryId, categories) => {};
+export const getContentNameById = (id, items = []) => {
+  if (!id && items.length === 0) {
+    return '';
+  }
+  const { name ='' } = items.find(category => category.id === id) || '';
+  return name;
+};
